625253af5efae202feefd52211752982f4f60c60
Tejasnavadkar <tejas.navadkar@gmail.com>
2026-01-10 23:51:00 +0530

    db schema setup

---
+++ backend/src/prismaClient.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {  // declare the global type declare global runtime ka nahi, sirf TypeScript ko samjhane ke liye 
+  // allow global caching in dev (HMR)
+  var __prisma: PrismaClient | undefined;
+}
+
+const options:ConstructorParameters<typeof PrismaClient>[0] = process.env.PRISMA_ACCELERATE_URL // is there prisma accelerate then use accelerate(cashing) otherwise use database url 
+  ? { accelerateUrl: process.env.PRISMA_ACCELERATE_URL }
+  : { adapter: { url: process.env.DATABASE_URL } };
+
+const prisma = global.__prisma ?? new PrismaClient(options); // Dev mode mein DB connection explosion se bachata hai
+
+if (process.env.NODE_ENV !== "production") global.__prisma = prisma;  // store instance in global cache to prevent multiple db connections
+
+export default prisma;
